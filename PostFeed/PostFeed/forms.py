2a388479d252d8b2584a9b559e2f39bf9cce760f
n1ght-lil-bugmaker <grachev.alekcandr@mail.ru>
2020-10-03 14:29:14 +0300

    fixed textarea
    теперь статью можно вводить нормально

--- PostFeed/PostFeed/forms.py
+++ PostFeed/PostFeed/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class PostForm(forms.Form):
     name = forms.CharField(max_length=30, label='Название')
     author = forms.CharField(max_length=25, label='Автор')
-    text = forms.Field()
+    text = forms.Field(label='Статья', widget=forms.Textarea)
     tags = forms.CharField(max_length=50, label='Тэги')
